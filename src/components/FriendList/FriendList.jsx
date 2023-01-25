7df7d125ae62a95b03a510c88a7668d57ec8a5af
Alina_Kuzyk <dr.alinakuzyk@gmail.com>
2023-01-25 08:17:20 +0100

    add propTypes to FriendsMenu/Item

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -5,12 +5,19 @@ export const FriendList = ({ friends }) => {
    return (
       <ul class="friend-list">
          {friends.map(({ id, avatar, name, isOnline }) => 
-            <FriendListItem id={id} avatar={avatar} name={name} status={isOnline} />)}
+            <FriendListItem
+               id={id}
+               avatar={avatar}
+               name={name}
+               status={isOnline} />)}
 </ul>
    )
 }
 
-// как прописать проптайпс для массива
-// FriendList.propTypes = {
-//    friends: PropTypes.array
-// }
+// proptypes в данном случае прописываем только для массива friends (arrayOf
+// - так как будем еще проверять id) и его свойство id(так как только он используються
+//  в данном файле).id входит в обьект - это будет PropTypes.shape, и  далее проверяем
+//  уже его значение
+FriendList.propTypes = {
+   friends: PropTypes.arrayOf(PropTypes.shape({id: PropTypes.number}))
+}
